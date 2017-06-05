44f92c20ac04d09810e2b24e16273956cbe42d2f
Pnevsk123 <vlad.007.p@gmail.com>
2017-06-05 14:49:02 +0300

    first commit

---
+++ Scripts/PlayerControls.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+    public float Speed;
+    private Rigidbody2D _rb;
+    public Borders _borders;
+    public GameObject Shot;
+    public Transform SpaunShotPoint;
+    public float FireRate;
+    private float NextFire;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time>NextFire)
+        {
+            NextFire = Time.time + FireRate;
+            Instantiate(Shot,new Vector3(SpaunShotPoint.position.x, SpaunShotPoint.position.y,0),Quaternion.Euler(0,0,0));
+
+
+
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(moveHorizontal, moveVertical, 0f);
+        transform.position += direction * Speed*Time.deltaTime;
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x,_borders.minx,_borders.maxx),
+           Mathf.Clamp(transform.position.y, _borders.miny, _borders.maxy),
+            transform.position.z);
+    }
+
+}
+
+
+
+[System.Serializable]
+public class Borders {
+
+    public float miny, maxy, minx, maxx;
+    
+}
---
+++ Scripts/RandomRotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+    public float angularSpeed = 20f;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().AddTorque(Random.Range(-angularSpeed, angularSpeed));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
